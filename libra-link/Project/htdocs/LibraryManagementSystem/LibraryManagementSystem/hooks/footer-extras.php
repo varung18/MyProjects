7e28a31b9af6549c562e6611626783e07bee5ccc
Varun Gaur <95574483+varung18@users.noreply.github.com>
2024-06-27 14:31:37 +0530

    Add files via upload

---
+++ libra-link/Project/htdocs/LibraryManagementSystem/LibraryManagementSystem/hooks/footer-extras.php
@@ -0,0 +1,6 @@
+<footer class="main-footer">
+    <div class="pull-right hidden-xs">
+    Developer: SVIT-Vasad
+    </div>
+    <strong>LibraLink</strong> 
+  </footer>
---
+++ libra-link/Project/htdocs/LibraryManagementSystem/LibraryManagementSystem/hooks/header-extras.php
@@ -0,0 +1,6 @@
+<footer class="main-header">
+    <div class="pull-right hidden-xs">
+    Developer: SVIT-Vasad
+    </div>
+    <strong>LibraLink</strong> 
+  </footer>
